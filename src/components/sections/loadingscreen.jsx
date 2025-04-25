4fb073ad8e6f1619c45d2c2c8c526efcf5fdc4c3
mohitjoer <mohitjoe.r@gmail.com>
2025-04-25 20:35:36 +0530

    commit	modified:   src/App.css commit	modified:   src/App.jsx commit	new file:   src/components/sections/about.jsx commit	new file:   src/components/sections/contact.jsx commit	new file:   src/components/sections/home.jsx commit	new file:   src/components/sections/loadingscreen.jsx commit	new file:   src/components/sections/project.jsx commit	modified:   src/index.css

---
+++ src/components/sections/loadingscreen.jsx
@@ -0,0 +1,16 @@
+export const LodingScreen=()=>{
+
+
+    return (
+        <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+            <div className="mb-4 text-4xl font-mono font-bold" >
+                hello world <span className="animate-blink ml-1">|</span>
+            </div>
+            <div className="w-[200px] bg-gray-800 rounded relative overflow-hidden">
+                <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loding-bar">
+                    {" "}
+                </div>
+            </div>
+        </div>
+    );
+};
