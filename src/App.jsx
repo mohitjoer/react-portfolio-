d2f4e52a4e91dbd2227853344028c634539daf02
mohitjoer <mohitjoe.r@gmail.com>
2025-05-01 22:31:47 +0530

    commit	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const lotteryNumbers = () => {
+    setCount(Math.floor(Math.random() * 900) + 100);
+    }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Lottery No</h1>
+    {count != 333 ? (
+      <h2>{count}</h2>
+    ) : (
+      <>
+        <h2>you won</h2>
+        <h3>{count}</h3>
+      </>
+    )}
+    <h2>Click the button to generate a random number</h2>
+    <button onClick={lotteryNumbers}>
+      Generate
+    </button>
     </>
   )
 }
